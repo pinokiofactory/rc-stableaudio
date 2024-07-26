362b6ef485425cee19b0f172c4d2a22877a118f1
cocktailpeanut <cocktailpeanuts@proton.me>
2024-07-26 07:36:41 -0400

    update

--- install.js
+++ install.js
@@ -61,6 +61,7 @@ module.exports = {
       method: "shell.run",
       params: {
         message: "huggingface-cli download RoyalCities/RC_Infinite_Pianos",
+        venv: "env",
         env: {
           HF_HUB_ENABLE_HF_TRANSFER: 1
         },
