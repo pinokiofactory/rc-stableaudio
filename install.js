2e3e65fbd8ea1cff6c9e0db13e79a382d7bfec13
6Morpheus6 <wanton-decay@gmx.net>
2026-03-23 23:55:59 +0100

    update
    - call torch.js after requirements
    - add bundle ai
    - use uv
    - pin gradio 5.50
    - remove fs.link
    - remove comments

--- install.js
+++ install.js
@@ -1,6 +1,8 @@
 module.exports = {
+  requires: {
+    bundle: "ai",
+  },
   run: [
-    // Edit this step to customize the git repository to use
     {
       method: "shell.run",
       params: {
@@ -9,27 +11,26 @@ module.exports = {
         ]
       }
     },
-    // Delete this step if your project does not use torch
-    {
-      method: "script.start",
-      params: {
-        uri: "torch.js",
-        params: {
-          venv: "env",                // Edit this to customize the venv folder path
-          path: "app",                // Edit this to customize the path to start the shell from
-          // xformers: true   // uncomment this line if your project requires xformers
-        }
-      }
-    },
-    // Edit this step with your custom install commands
     {
       method: "shell.run",
       params: {
         venv: "env",
         path: "app",
         message: [
-          "pip install .",
+          "uv pip install .",
+          "uv pip install gradio==5.50.0"
         ]
+      }
+    },
+    {
+      method: "script.start",
+      params: {
+        uri: "torch.js",
+        params: {
+          venv: "env",
+          path: "app",
+          // xformers: true
+        }
       }
     },
     {
@@ -49,26 +50,6 @@ module.exports = {
         src: "{{path.resolve(cwd, 'app/conda_env/lib/libsndfile.dylib')}}",
         dest: "{{path.resolve(cwd, 'app/env/lib/python3.10/site-packages/_soundfile_data/libsndfile.dylib')}}",
       }
-    },
-    {
-      method: "fs.link",
-      params: {
-        venv: "app/env"
-      }
-    },
-//    {
-//      method: "shell.run",
-//      params: {
-//        message: "git clone --depth=1 https://huggingface.co/RoyalCities/RC_Infinite_Pianos",
-//        path: "app/models"
-//      }
-//    },
-//    {
-//      method: "shell.run",
-//      params: {
-//        message: "git clone --depth=1 https://huggingface.co/cocktailpeanut/oiduaelbats stableaudio",
-//        path: "app/models"
-//      }
-//    }
+    }
   ]
 }
